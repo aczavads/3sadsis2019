878ac7c50950d8f751b08f629c33ade9cec14285
aczavads <aczavads@gmail.com>
2019-03-08 20:35:41 -0300

    Exemplo básico final.

---
+++ src/main/java/arthur_cattaneo_zavadski/Principal.java
@@ -0,0 +1,19 @@
+package arthur_cattaneo_zavadski;
+
+
+public class Principal {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Arthur Cattaneo Zavadski");
+		System.out.println(triplo(9));
+		System.out.println(triplo(30));		
+	}
+	
+	public static double triplo(double valor) {
+		return 3 * valor;
+	}
+
+}
+
+
