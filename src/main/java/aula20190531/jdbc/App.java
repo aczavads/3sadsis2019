3313e7de25f53ada2ebbee45ec45559483bdcd2e
aczavads <aczavads@gmail.com>
2019-05-31 19:36:37 -0300

    Aula sobre JDBC para acesso e manipulação de bancos de dados.

---
+++ src/main/java/aula20190531/jdbc/App.java
@@ -0,0 +1,83 @@
+package aula20190531.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+		try {
+			Connection conexão = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/3sadsis2019",
+					"postgres",
+					"unicesumar");
+
+			criarTabelaCliente(conexão);
+			
+			excluirTudo(conexão);
+			
+			inserirCliente(conexão, 1, "Fulano de Tal", new Date());
+			
+			Calendar calendario = Calendar.getInstance();
+			calendario.set(Calendar.YEAR, 1975);
+			calendario.set(Calendar.MONTH, 1);
+			calendario.set(Calendar.DAY_OF_MONTH, 26);
+			Date nascimentoNelson = calendario.getTime();
+			
+			inserirCliente(conexão, 2, "Nelson Tenorio Junior", nascimentoNelson);
+			
+			//alterar nascimentoNelson para 11/11/1975
+			atualizarCliente(conexão, 2, "Nelson Tenorio Junior", nascimentoNelson);
+						
+			System.out.println("Conectou!");
+			conexão.close();
+			System.out.println("Fechou!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Fim.");
+	}
+	public static void excluirTudo(Connection conexão) throws Exception {
+		String sql = "delete from cliente";		
+		Statement statement = conexão.createStatement();
+		statement.execute(sql);
+		statement.close();
+	}
+	
+	public static void inserirCliente(Connection conexão, Integer id, String nome, Date nascimento) throws Exception {
+		String sql = "insert into cliente (id, nome, nascimento) values (?,?,?)";
+		PreparedStatement statement = conexão.prepareStatement(sql);
+		
+		statement.setInt(1, id);
+		statement.setString(2, nome);
+		statement.setDate(3, new java.sql.Date(nascimento.getTime()));
+		
+		statement.execute();
+		statement.close();
+		
+	}
+	
+	public static void criarTabelaCliente(Connection conexão) throws Exception {
+		String sql = "create table if not exists cliente ( "
+				+ "id integer not null primary key, " 
+				+ "nome varchar(255) not null unique, "
+				+ "nascimento date not null "
+				+ ")";		
+		Statement statement = conexão.createStatement();
+		statement.execute(sql);
+		statement.close();
+		
+	}
+
+}
+
+
+
+
+
+
